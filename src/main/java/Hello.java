a27da3a8a62c3e92c6edc3704bef4176ce33f70e
pimta <pimtapim@hotmail.com>
2016-08-26 20:47:18 +0700

    Update Hello.java
    asdf

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,11 +13,24 @@ public class Hello {
 
 	public static String greetingMessage(String name){
 		// TODO Return string in this format - "Hello, " + given name.
-		return "";
+		return ("Hello"+name;
 	}
 
 	public static int[] bubbleSort(int[] array){
 		// TODO Sort the given array in an ascending order and return the sorted array.
+		Boolean isSwap;
+		do{
+			is Swap = false;
+			for (int i = 0; i<array.length-1; i++){
+				if (array[i])>array[i+1]) {
+					int temp = array[i];
+					array[i] = array[i+1];
+					array[i+1]=temp;
+					isSwap = true;
+				}
+			}
+		}while(isSwap);
+		
 		return array;
 	}
 	
